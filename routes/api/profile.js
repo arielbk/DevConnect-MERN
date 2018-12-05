ffd164cda984765892a680857a72a4597b0b8301
arielbk <buchwald.kearney@gmail.com>
2018-12-05 21:24:56 +0100

    add api to retrieve all profiles, by id and by handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -34,6 +34,63 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     .catch(err => res.status(404).json(err));
 });
 
+// @route   GET api/profile/all
+// @desc    Get all profiles
+// @access  Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.profiles = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(err => {
+      errors.profiles = 'There are no profiles';
+      return res.status(404).json(errors);
+    });
+});
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no user with that handle';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(error => res.status(404).json(error));
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no user with that id';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(() => res.status(404).json({ profile: 'There is no user with that id' }));
+});
+
 // @route   POST api/profile/
 // @desc    Create or edit user profile
 // @access  Private
