ed2fcaf74955d6cf3a16ed04b3a24e73dad40567
arielbk <buchwald.kearney@gmail.com>
2018-12-14 15:55:48 +0100

    add main react routes

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>REgister</h1>
+      </div>
+    )
+  }
+};
+
+export default Register;
