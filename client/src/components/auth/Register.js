518288802a2d8c4c3972d08e7d0a1d40d7203d92
arielbk <buchwald.kearney@gmail.com>
2018-12-15 18:44:53 +0100

    add register and login pages

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,107 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+    errors: {},
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, password, password2 } = this.state;
+    const newUser = {
+      name,
+      email,
+      password,
+      password2
+    };
+
+    // Register the user... (without redux for now)
+    axios.post('api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
+  }
+
   render() {
+    const { name, email, password, password2, errors } = this.state;
     return (
-      <div>
-        <h1>REgister</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">Create your DevConnector account</p>
+              <form noValidate onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <input
+                    value={name}
+                    onChange={this.handleChange}
+                    type="text"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
+                    placeholder="Name"
+                    name="name"
+                    required
+                  />
+                  {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    value={email}
+                    onChange={this.handleChange}
+                    type="email"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
+                    placeholder="Email Address"
+                    name="email"
+                  />
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                </div>
+                <div className="form-group">
+                  <input
+                    value={password}
+                    onChange={this.handleChange}
+                    type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
+                    placeholder="Password"
+                    name="password"
+                  />
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+                </div>
+                <div className="form-group">
+                  <input
+                    value={password2}
+                    onChange={this.handleChange}
+                    type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
+                    placeholder="Confirm Password"
+                    name="password2"
+                  />
+                  {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
